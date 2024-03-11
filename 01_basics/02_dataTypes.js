4eabb157674a39119be8bd9bd27f0b1b37d93198
shivamjdd <85242128+shivamjdd@users.noreply.github.com>
2024-03-11 09:44:21 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as a newer version
+
+// alert ( 3+ 3 ) we are using nodejs , not browser
+// code readability should be high 
+
+let name = "Shivam";
+let age = 18
+let isLoggedIn = false 
+
+// number => 2 to powe 53 
+// bigint
+//string => ""
+// bollean => true/false 
+// null => standalone value
+// undefined => Abhi value assign nhi huyi hy 
+// symbol => unique 
+// object 
+
+console.log( type of shivam);
