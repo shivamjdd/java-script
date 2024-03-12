dbbb4a99a6a1d6433501e61cb10fc91051b93c83
shivamjdd <85242128+shivamjdd@users.noreply.github.com>
2024-03-12 05:22:28 +0000

    conversationoperation.js

---
+++ 01_basics/03_conversationoperation.js
@@ -0,0 +1,25 @@
+let score = "33";
+
+let valueInNumber = Number(score);
+// console.log( typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = "Shivam"
+
+let bolleanIsLoggedIn = Boolean(isLoggedIn);
+console.log( bolleanIsLoggedIn);
+
+// 1 => true ; 0=> false
+// "" => false 
+// "Shivam" => true
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
