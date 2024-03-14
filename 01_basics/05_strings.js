993632804c38bdb5395a95346f9e985345fc2f67
shivamjdd <85242128+shivamjdd@users.noreply.github.com>
2024-03-14 11:46:25 +0000

    05_strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,31 @@
+const name = "Shivam";
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String("New-Dragon-com");
+// console.log(gameName.__proto__);
+
+// console.log(gameName.charAt(3));
+// console.log(gameName.lastIndexOf('t'))
+
+const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8 ,7)
+// console.log(anotherString);
+
+// const newStringOne = "    shivam    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://shivam.com/shivam%20gupta"
+
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('sundar'));
+
+
+console.log(gameName.split('-'));
