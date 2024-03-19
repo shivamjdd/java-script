fc1a39f87cd32313c0f80021aade798dcbbd0170
shivamjdd <85242128+shivamjdd@users.noreply.github.com>
2024-03-19 13:33:41 +0000

    04_objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,49 @@
+// const tinderUser = new Object()  // singleton object
+const tinderUser = {}  //
+
+tinderUser.id = "123abc"
+tinderUser.name = "Rohan"
+tinderUser.isLoggedIn = false 
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "shivam",
+            lastname: "gupta"
+        }
+
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.lastname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = Object.assign(obj1, obj2)
+// const obj3 = Object.assign({}, obj1, obj2) // guranteed 
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "xyz@gmail.com",
+    },
+    {
+        id: 1,
+        email: "xyz@gmail.com",
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('LoggedIn'));
