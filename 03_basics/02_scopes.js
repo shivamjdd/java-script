9cca3b6c40a410efe1fc8c44028390348be82eee
shivamjdd <85242128+shivamjdd@users.noreply.github.com>
2024-03-21 07:38:25 +0000

    02_scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,13 +1,15 @@
-let a = 10
-const b = 20
-var c = 30
+let a = 300
+
+// var c = 30
 
 if(true) {
     let a = 10
     const b = 20
-    var c = 30
+    // var c = 30
+    console.log("Inner:", a);
 }
 
-// console.log(a);
+console.log(a);
 // console.log(b);
 // console.log(c);
+
