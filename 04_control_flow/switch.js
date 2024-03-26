ded70e31fe4afcec05eaad1e18d0510750b6c7be
shivamjdd <85242128+shivamjdd@users.noreply.github.com>
2024-03-26 11:28:20 +0000

    switch.js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 8
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+        case 5:
+            console.log("May");
+            break;
+        case 6:
+            console.log("June");
+            break;
+        case 7:
+            console.log("July");
+            break;
+        case 8:
+            console.log("August");
+            break;
+            case 9:
+                console.log("Sept");
+                break;
+            case 10:
+                console.log("Oct");
+                break;
+            case 11:
+                console.log("November");
+                break;
+            case 12:
+                console.log("Dec");
+                break;
+
+    default:
+        console.log("Month is not correct please check that")
+        break;
+}
