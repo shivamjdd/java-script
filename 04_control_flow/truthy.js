9ee4ff48dfe7ac7d0dd7ef09660f7e735448566b
shivamjdd <85242128+shivamjdd@users.noreply.github.com>
2024-03-26 11:28:42 +0000

    truthly.js

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,43 @@
+// const userEmail = ''
+
+// if (userEmail) {
+//     console.log("got user email");
+// } else {
+//     console.log("Dont have user email");
+// }
+
+// falsy value
+
+// false , 0 , -0 , BigInt 0n , "" , null , undefined , NaN
+
+// truthy value 
+// "0", "false", " ", [], {}, function() {}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+// const emptyObject = {}
+// if (Object.keys(emptyObject).length === 0) {
+//     console.log("Object is empty");
+// }
+
+
+// Nullish CoaLESING Operator (??): null undefined
+
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 15
+
+// console.log(val1);
+
+
+// Terniary Operator 
+
+// condition ? true : false
+
+const iceTeaPrize = 100
+iceTeaPrize <= 80 ? console.log("less than 80") : console.log("more than 80");
+
