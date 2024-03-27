63b527a31ea786711efed8c5af2be10498554826
shivamjdd <85242128+shivamjdd@users.noreply.github.com>
2024-03-27 11:36:40 +0000

    four.js

---
+++ 05_iterations/four.js
@@ -0,0 +1,24 @@
+const myObject = {
+    js: 'Javascript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    py: 'Python'
+} 
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ['js','rb','py','java','cpp']
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', 'India')
+// map.set('USA', 'United states of America')
+// map.set('Fr', 'France')
+
+// for (const key in map) {
+//     console.log(key);
+// }
