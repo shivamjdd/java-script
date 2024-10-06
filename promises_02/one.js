4c8175134540beca68f7e079a1af47e4a8ff1268
Shivam Gupta <85242128+shivamjdd@users.noreply.github.com>
2024-10-06 19:24:06 +0000

    one.js

--- promises_02/one.js
+++ promises_02/one.js
@@ -2,7 +2,13 @@ function walkDog() {
     
     return new Promise((resolve,reject)=> {
         setTimeout(()=>{
+          const dogWaled = true;
+          if(dogWaled){
             resolve("You walk the dog🦮")
+          } else{
+            reject("You didn't walk the dog")
+          }
+            
             
           }, 1500);
     })
@@ -13,7 +19,13 @@ function walkDog() {
 
     return new Promise((resolve,reject) => {
         setTimeout(()=>{
+          const kitchenCleaned = true;
+          if(kitchenCleaned){
             resolve("You clean the kitchen🍷")
+          }else{
+            reject("You didn't cleaned kitchen")
+          }
+            
             
           }, 2500);
     })
@@ -22,10 +34,18 @@ function walkDog() {
   function takeOutTrash() {
     
     return new Promise((resolve,reject) => {
+      
         setTimeout(()=>{
-            resolve("Take out the trash🕸")
+          const trashTakenOut = false;
+        if(trashTakenOut){
+          resolve("Take out the trash🕸")
+        }else{
+          reject("You didn't take out trash ")
+        }
+            
           }, 500);
     })
   }
   
-  walkDog().then(value => {console.log(value); return CleanKitchen().then(value => {console.log(value); return takeOutTrash().then(value => {console.log(value)})})})
+  walkDog().then(value => {console.log(value); return CleanKitchen().then(value => {console.log(value); return takeOutTrash().then(value => {console.log(value); console.log("You done all the tasks") })})})
+  .catch((error)=>{console.error(error)});
